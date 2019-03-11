a700e0b0423811d417cc176793cf2542cadd17ec
RobinLiu <liulongyang0305@126.com>
2019-03-11 12:51:54 +0800

    Update Tree.swift

--- swift/Tree.swift
+++ swift/Tree.swift
@@ -1333,6 +1333,39 @@ class CBTInserter {
         }
     }
 }
+func convertBSTRecursion(_ root: TreeNode?) -> TreeNode? {
+    var sum : Int = 0
+    func convertBSTInternal(_ root : TreeNode?) -> TreeNode?{
+        guard let node = root else {
+            return nil
+        }
+        convertBSTInternal(node.right)
+        sum += node.val
+        node.val = sum
+        convertBSTInternal(node.left)
+        return root
+    }
+    return convertBSTInternal(root)
+}
+
+func convertBSTIteration(_ root : TreeNode?)-> TreeNode? {
+    if nil != root {
+        var temp : TreeNode? = root
+        var sum : Int = 0
+        var stack = Array<TreeNode?>()
+        while !stack.isEmpty || nil != temp {
+            while nil != temp {
+                stack.append(temp)
+                temp = temp?.right
+            }
+            temp = stack.removeLast()
+            sum += (temp?.val)!
+            temp?.val = sum
+            temp = temp?.left
+        }
+    }
+    return root
+}
 
 
 
