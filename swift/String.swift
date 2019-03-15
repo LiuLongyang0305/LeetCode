9a28932714feb4d22af491fcb7d602de034da4c6
RobinLiu <liulongyang0305@126.com>
2019-03-15 11:03:14 +0800

    Update String.swift

--- swift/String.swift
+++ swift/String.swift
@@ -530,3 +530,30 @@ class Q657_Solution {
         return stepCounter.Right == 0 && stepCounter.Up == 0
     }
 }
+class Q680_Solution {
+    func validPalindrome(_ s: String) -> Bool {
+        let chars = Array<Character>(s)
+        var i = 0
+        var j = chars.count - 1
+        while i < j {
+            if chars[i] != chars[j] {
+                return isPalindromeRange(arr: chars, start: i + 1, end: j) || isPalindromeRange(arr: chars, start: i, end: j - 1)
+            }
+            i += 1
+            j -= 1
+        }
+        return true
+    }
+    private func isPalindromeRange(arr : [Character], start : Int, end : Int) -> Bool {
+        var i = start
+        var j = end
+        while i < j {
+            if arr[i] != arr[j] {
+                return false
+            }
+            i += 1
+            j -= 1
+        }
+        return true
+    }
+}
