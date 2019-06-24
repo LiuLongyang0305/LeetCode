6ce44aba65f620d6ebd682d2bc0708de375a4bd8
RobinLiu <liulongyang0305@126.com>
2019-06-24 15:50:15 +0800

    Create Q1095_FindInMountainArray.c

---
+++ cpp/Q1095_FindInMountainArray.c
@@ -0,0 +1,84 @@
+//https://leetcode.com/problems/find-in-mountain-array/
+/**
+ * *********************************************************************
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * *********************************************************************
+ */
+ int get(MountainArray *, int index);
+ int length(MountainArray *);
+ 
+int findInMountainArray(int target, MountainArray* mountainArr){
+	int  arrLength = length(mountainArr);
+	int arr[arrLength];
+	for (int i = 0; i < arrLength; ++i)
+	{
+		arr[i] = 0;
+	}
+
+	int targetIndex = arrLength / 2;
+	int deltaIndex = arrLength / 2;
+	while(1) {
+		deltaIndex /=  2;
+		int first = get(mountainArr,targetIndex - 1);
+		int third = get(mountainArr,targetIndex + 1);
+		int second = get(mountainArr,targetIndex);
+		arr[targetIndex - 1] = first;
+		arr[targetIndex] = second;
+		arr[targetIndex + 1 ] = third;
+		if (first < second && second > third) {
+			break;
+		} else if (first < second && second < third) {
+			targetIndex += deltaIndex;
+		} else {
+			targetIndex -= deltaIndex;
+		}
+	}
+
+	if(arr[targetIndex] < target) {
+		return -1;
+	} else if (arr[targetIndex] == target) {
+		return targetIndex;
+	}
+    // printf("****%d",targetIndex);
+	int low = 0;
+	int high = targetIndex - 1;
+	while (low < high && low >= 0) {
+		int mid = low + (high - low) >> 1;
+		int temp = get(mountainArr,mid);
+		if(temp == target) {
+			return mid;
+		} else if(temp > target) {
+			high = mid - 1;
+		} else {
+			low = mid + 1;
+		}
+	}
+	if (low == high) {
+		if (get(mountainArr,low) == target) {
+			return low;
+		}
+	}
+
+	low = targetIndex + 1;
+	high = arrLength - 1;
+	while (low < high && high < arrLength) {
+		int mid = low + (high - low) >> 1;
+		int temp = get(mountainArr,mid);
+		if(temp == target) {
+			return mid;
+		} else if(temp > target) {
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
+	}
+	if (low == high) {
+		if (get(mountainArr,low) == target) {
+			return low;
+		}
+	}
+	return -1;
+}
+
+
