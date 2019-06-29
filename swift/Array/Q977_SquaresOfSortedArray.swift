eda2e3f57a07131078e3b84d20bd52d80d604ec8
RobinLiu <liulongyang0305@126.com>
2019-06-29 22:20:08 +0800

    Create Q977_SquaresOfSortedArray.swift

---
+++ swift/Array/Q977_SquaresOfSortedArray.swift
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/squares-of-a-sorted-array/
+class Solution {
+    func sortedSquares(_ A: [Int]) -> [Int] {
+        var result = [Int]()
+        if A[0] >= 0 {
+            for ele in A {
+                result.append(ele * ele)
+            }
+            return result
+        }
+        if A[A.count - 1] <= 0 {
+            for ele in A {
+               result.insert(ele * ele, at: 0)
+            }
+             return result
+        }
+        var i = 0
+        var j = A.count - 1
+        while i <= j {
+            let a = A[i]
+            let b = A[j]
+            if abs(a) < abs(b) {
+                result.insert(b * b, at: 0)
+                j -= 1
+            } else {
+                result.insert(a * a, at: 0)
+                i += 1
+            }
+        }
+        return result
+    }
+}
