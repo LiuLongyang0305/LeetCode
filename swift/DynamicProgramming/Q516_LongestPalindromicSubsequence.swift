9545f8615cc7b76aef0265da58a1fbc313bf31a2
RobinLiu <liulongyang0305@126.com>
2019-07-04 19:35:43 +0800

    Create Q516_LongestPalindromicSubsequence.swift

---
+++ swift/DynamicProgramming/Q516_LongestPalindromicSubsequence.swift
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/longest-palindromic-subsequence/
+class Solution {
+    func longestPalindromeSubseq(_ s: String) -> Int {
+        let size = s.count
+        let stringToArray = Array<Character>(s)
+        var dp = Array<Array<Int>>(repeating: Array<Int>(repeating: 0, count: size), count: size)
+        var i = size - 1
+        while i >= 0 {
+            dp[i][i] = 1
+            for j in (i + 1)..<size {
+                if stringToArray[i] == stringToArray[j] {
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+                }
+            }
+            i -= 1
+        }
+        return dp[0][size - 1]
+    }
+}
