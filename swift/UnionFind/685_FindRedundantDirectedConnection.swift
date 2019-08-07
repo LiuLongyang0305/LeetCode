1ed424edca275a256edfc38584ae8a7b23525afd
RobinLiu <liulongyang0305@126.com>
2019-08-07 14:55:56 +0800

    Update 685_FindRedundantDirectedConnection.swift

--- swift/UnionFind/685_FindRedundantDirectedConnection.swift
+++ swift/UnionFind/685_FindRedundantDirectedConnection.swift
@@ -43,3 +43,87 @@ class Solution {
         return potentionEdge.isEmpty ? [secondParent,target] : potentionEdge
     }
 }
+
+
+class Solution2 {
+    typealias Degree = (in1: Int,out: Int)
+    func findRedundantDirectedConnection(_ edges: [[Int]]) -> [Int] {
+        var reverseAdjancenyList = Array<Array<Int>>(repeating: Array<Int>(), count: edges.count + 1)
+        var adjancenyList = Array<Array<Int>>(repeating: Array<Int>(), count: edges.count + 1)
+        var degrees = Array<Degree>(repeating: (0,0), count: edges.count + 1)
+        for edge in edges {
+            degrees[edge[0]].out += 1
+            degrees[edge[1]].in1 += 1
+            reverseAdjancenyList[edge[1]].append(edge[0])
+            adjancenyList[edge[0]].append(edge[1])
+        }
+        
+        func valid(parent: Int, child: Int) -> Bool {
+            
+            var tempDegrees = degrees
+            var tempAdjacencyList = adjancenyList
+            
+            tempDegrees[parent].out -= 1
+            tempDegrees[child].in1 -= 1
+            let children = tempAdjacencyList[parent]
+            print([parent,child])
+            for j in 0..<children.count {
+                if children[j] == child {
+                    tempAdjacencyList[parent].remove(at: j)
+                    break
+                }
+            }
+            
+            var ans = 0
+            var  queue = [Int]()
+            for i in 1...edges.count {
+                if tempDegrees[i].in1 == 0 {
+                    queue.append(i)
+                }
+            }
+            
+            if queue.count != 1 {
+                return false
+            }
+            
+            var exisited = Set<Int>()
+            while !queue.isEmpty {
+                let length = queue.count
+                ans += length
+                for _ in 0..<length {
+                    let currentNode = queue.removeFirst()
+                    exisited.insert(currentNode)
+                    for node in tempAdjacencyList[currentNode] {
+                        if !exisited.contains(node) {
+                            queue.append(node)
+                        }
+                    }
+                }
+            }
+            return  ans == edges.count
+        }
+        
+        for i in 1...edges.count {
+            if degrees[i].in1 == 2 {
+                let parents = reverseAdjancenyList[i].reversed()
+                for parent in parents {
+                    if valid(parent: parent, child: i) {
+                        return [parent,i]
+                    }
+                }
+            }
+        }
+        
+        for i in (0..<edges.count).reversed() {
+            var fromDegree = degrees[edges[i][0]]
+            var toDegree = degrees[edges[i][1]]
+            fromDegree.out -= 1
+            toDegree.in1 -= 1
+            if  toDegree.in1 != 0 || toDegree.out != 0 {
+                return edges[i]
+            }
+        }
+        
+        return [Int]()
+    }
+}
