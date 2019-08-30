a631c82b6f3905330555e5af1492c3037f91eb30
RobinLiu <liulongyang0305@126.com>
2019-08-30 17:13:17 +0800

    Update 190_ReverseBits.c

--- cpp/190_ReverseBits.c
+++ cpp/190_ReverseBits.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
 uint32_t reverseBits(uint32_t n) {
-    
+    uint32_t arr[32] = {0};
+    for (int i = 0; i < 32; ++i)
+    {
+    	arr[i]  = n & 1;
+    	n >>= 1;
+    }
+    uint32_t ans = 0;
+    uint32_t base = 1;
+    for (int i = 31; i >= 0; --i)
+    {
+    	ans += (arr[i] * base);
+    	base *= 2;
+    }
+    return ans;
 }
