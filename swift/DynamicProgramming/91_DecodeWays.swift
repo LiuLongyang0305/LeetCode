e28704da33bd4fa8eff8d1abd307c2ddae16bcf0
RobinLiu <liulongyang0305@126.com>
2019-10-18 12:34:34 +0800

    Update 91_DecodeWays.swift

--- swift/DynamicProgramming/91_DecodeWays.swift
+++ swift/DynamicProgramming/91_DecodeWays.swift
@@ -24,3 +24,21 @@ class Solution {
         return current
     }
 }
+class Solution2 {
+    func numDecodings(_ s: String) -> Int {
+        let chars = s.map{ Int($0.asciiValue! - zeroAsciiVal)}
+        guard chars[0] != 0 else {
+            return 0
+        }
+        var pre = 1
+        var current = 1
+        for i in 1..<s.count {
+            let temp = current
+            guard !(chars[i] == 0  && !(chars[i - 1] == 1 || chars[i - 1] == 2)) else {
+                return 0
+            }
+            (current,pre) = (chars[i] == 0 ? pre : chars[i - 1] == 1 || chars[i - 1]  == 2 && chars[i] >= 1 && chars[i] <= 6 ? (current + pre) : current, temp )
+        }
+        return current
+    }
+}
