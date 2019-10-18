476f7e0e4a60b8abccedff24e7554ee80044239b
RobinLiu <liulongyang0305@126.com>
2019-10-18 12:26:44 +0800

    Create 91_DecodeWays.swift

---
+++ swift/DynamicProgramming/91_DecodeWays.swift
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/decode-ways/
+let zeroAsciiVal = Character("0").asciiValue!
+class Solution {
+    func numDecodings(_ s: String) -> Int {
+        let chars = s.map{ Int($0.asciiValue! - zeroAsciiVal)}
+        if chars[0] == 0 {
+            return 0
+        }
+        var pre = 1
+        var current = 1
+        for i in 1..<s.count {
+            let temp = current
+            if chars[i] == 0 {
+                if chars[i - 1] == 1 || chars[i - 1] == 2 {
+                    current = pre
+                } else {
+                    return 0
+                }
+            } else if chars[i - 1] == 1 || chars[i - 1]  == 2 && chars[i] >= 1 && chars[i] <= 6 {
+                current = current + pre
+            }
+            pre = temp
+        }
+        return current
+    }
+}
