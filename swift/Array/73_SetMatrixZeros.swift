f712578487374c17548ec286b05777b797fd6a55
RobinLiu <liulongyang0305@126.com>
2019-10-29 00:09:25 +0800

    Update 73_SetMatrixZeros.swift

--- swift/Array/73_SetMatrixZeros.swift
+++ swift/Array/73_SetMatrixZeros.swift
@@ -23,3 +23,35 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    func setZeroes(_ matrix: inout [[Int]]) {
+        
+        let row = matrix.count
+        let col = matrix[0].count
+        
+        for i in 0..<row {
+            for j in 0..<col {
+                if matrix[i][j] == 0 {
+                    for k in 0..<col {
+                        if matrix[i][k] != 0 {
+                            matrix[i][k] = Int.max
+                        }
+                    }
+                    for k in 0..<row {
+                        if matrix[k][j] != 0 {
+                            matrix[k][j] = Int.max
+                        }
+                    }
+                }
+            }
+        }
+        for i in 0..<row {
+            for j in 0..<col {
+                if matrix[i][j] == Int.max {
+                    matrix[i][j] = 0
+                }
+            }
+        }
+    }
+}
