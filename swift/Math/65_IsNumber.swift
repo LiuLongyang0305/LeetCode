5839619f6267b026c3f2680069f5d4ca1e83efa7
RobinLiu <liulongyang0305@126.com>
2019-11-17 23:41:39 +0800

    Create 65_IsNumber.swift

---
+++ swift/Math/65_IsNumber.swift
@@ -0,0 +1,60 @@
+//https://leetcode.com/problems/valid-number/
+class Solution {
+    func isNumber(_ s: String) -> Bool {
+        var sCopy = s
+        while !sCopy.isEmpty  && sCopy.first! == " " {
+            sCopy.removeFirst()
+        }
+        while !sCopy.isEmpty  && sCopy.last! == " " {
+            sCopy.removeLast()
+        }
+        guard !sCopy.isEmpty else {
+            return  false
+        }
+        if sCopy.first!  == "-" || sCopy.first!  == "+" {
+            sCopy.removeFirst()
+        }
+        guard !sCopy.isEmpty else {
+            return  false
+        }
+        guard !sCopy.hasPrefix("-")  && !sCopy.hasPrefix("-") else {
+            return false
+        }
+        if let index = sCopy.firstIndex(of: "e") {
+            let left = String(sCopy[sCopy.startIndex..<index])
+            let right = String(sCopy[sCopy.index(after: index)..<sCopy.endIndex ])
+            print("\(isInt(left))   \(isInt(right))")
+            return (isInt(left) || isDouble(left)) && isInt(right)
+        }
+
+        return isDouble(sCopy)  || isInt(sCopy)
+    }
+    private func  isInt(_ str: String)  -> Bool {
+        var sCopy = str
+        while sCopy.hasPrefix("0") {
+            sCopy.removeFirst()
+        }
+        return Int(str) != nil
+    }
+    private func isDouble(_ str: String)  -> Bool {
+
+        if str.hasPrefix(".") || str.hasSuffix("."){
+            var strCopy = str
+            if strCopy.hasPrefix(".") {
+            strCopy.removeFirst()
+            } else {
+                strCopy.removeLast()
+            }
+            guard !strCopy.hasPrefix("-") && !strCopy.hasPrefix("+") else {
+                return false
+            }
+            return isInt(strCopy) ? Int(strCopy)! >= 0 : false
+        }
+        
+        if  let index  = str.firstIndex(of: "."){
+            let  rightStr = String(str[str.index(after: index)..<str.endIndex])
+            return isInt(String(str[str.startIndex..<index]))  && isInt(rightStr) && Int(rightStr)!  >= 0
+        }
+        return  false
+    }
+}
