337711e138404593e742a4f2a421293ddf4bc99f
RobinLiu <liulongyang0305@126.com>
2020-02-23 15:41:18 +0800

    Create 1358_NumberOfSubstringsContainingAllThreeCharacters.swift

---
+++ swift/String/1358_NumberOfSubstringsContainingAllThreeCharacters.swift
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/ 
+class Solution {
+    
+    func numberOfSubstrings(_ s: String) -> Int {
+        var ans = 0
+        
+        let chars = [Character](s)
+        var lastIndexOfA = -1
+        var lastIndexOfB = -1
+        var lastIndexOfC = -1
+        
+        for index in 0..<s.count {
+            switch chars[index] {
+            case "a":
+                lastIndexOfA = index
+            case "b":
+                lastIndexOfB = index
+            case "c":
+                lastIndexOfC = index
+            default:
+                break
+            }
+            let minIndex = min(lastIndexOfB, lastIndexOfA, lastIndexOfC)
+            if  minIndex != -1 {
+                ans += minIndex + 1
+            }
+        }
+        return ans
+    }
+ }
+ 
