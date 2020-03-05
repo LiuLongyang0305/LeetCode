423f0023162977976f88c8a7f56290da4b04effc
RobinLiu <liulongyang0305@126.com>
2020-03-05 23:31:28 +0800

    Update 1208_GetEqualSubstringsWithinBudget.swift

--- swift/BinarySearch/1208_GetEqualSubstringsWithinBudget.swift
+++ swift/BinarySearch/1208_GetEqualSubstringsWithinBudget.swift
@@ -55,3 +55,25 @@ class Solution {
         return maxLength
      }
  }
+
+  class Solution {
+    func equalSubstring(_ s: String, _ t: String, _ maxCost: Int) -> Int {
+        let s2Nums = s.map { Int($0.asciiValue!)}
+        let t2Nums = t.map { Int($0.asciiValue!)}
+        var ans = 0
+        var cost = 0
+        let N = s.count
+        var left = 0
+        var right = 0
+        while right < N {
+            cost += abs(s2Nums[right] - t2Nums[right])
+            while cost > maxCost {
+                cost -= abs(s2Nums[left] - t2Nums[left])
+                left += 1
+            }
+            ans = max(ans, right - left + 1)
+            right += 1
+        }
+        return ans
+    }
+ }
