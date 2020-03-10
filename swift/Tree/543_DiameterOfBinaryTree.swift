ff39cede7a352ae011c12c40b2367fd2e22d9a3c
RobinLiu <liulongyang0305@126.com>
2020-03-10 16:40:58 +0800

    Update 543_DiameterOfBinaryTree.swift

--- swift/Tree/543_DiameterOfBinaryTree.swift
+++ swift/Tree/543_DiameterOfBinaryTree.swift
@@ -31,3 +31,20 @@ class Q543_Solution {
         return maxLength - 1
     }
 }
+
+ class Solution {
+    func diameterOfBinaryTree(_ root: TreeNode?) -> Int {
+        var diameter = 0
+        func dfs(_ root: TreeNode?) -> Int {
+            guard let root = root else {
+                return 0
+            }
+            let left = dfs(root.left)
+            let right = dfs(root.right)
+            diameter = max(diameter, left + right)
+            return max(left, right)
+        }
+        dfs(root)
+        return diameter
+    }
+ }
