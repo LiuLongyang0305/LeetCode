861e13fde589800b3e2b4f3b5b62f4d844156478
RobinLiu <liulongyang0305@126.com>
2020-03-23 20:50:09 +0800

    Create 1390_FourDivisors.swift

---
+++ swift/Math/1390_FourDivisors.swift
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/four-divisors/
+class Solution {
+    func sumFourDivisors(_ nums: [Int]) -> Int {
+        var ans = 0
+        func fourDivisorsSum(of num: Int) -> Int? {
+            var divisorsCount = 0
+            var ans = 0
+            for i in 1...Int(sqrt(Double(num))) {
+                if num % i == 0 {
+                    let another = num / i
+                    if i == another {
+                        ans += i
+                        divisorsCount += 1
+                    } else {
+                        ans += i + another
+                        divisorsCount += 2
+                    }
+                    guard divisorsCount <= 4 else {
+                        return nil
+                    }
+                }
+            }
+            
+            return divisorsCount == 4 ? ans : nil
+        }
+        for num in nums {
+            if let s = fourDivisorsSum(of: num) {
+                ans += s
+            }
+        }
+        return ans
+    }
+}
+
