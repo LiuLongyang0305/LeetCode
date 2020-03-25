523b8107fa5c481623198955e1752282e7076fdc
RobinLiu <liulongyang0305@126.com>
2020-03-25 13:46:32 +0800

    Create 371_SumOfTwoIntegers.swift

---
+++ swift/BitMapipulation/371_SumOfTwoIntegers.swift
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/sum-of-two-integers/
+class Solution {
+    func getSum(_ a: Int, _ b: Int) -> Int {
+        return b == 0 ? a : getSum(a ^ b, (a & b) << 1)
+    }
+}
