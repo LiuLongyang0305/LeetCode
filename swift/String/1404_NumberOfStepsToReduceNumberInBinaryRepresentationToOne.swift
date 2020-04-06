b7e0aa0d3102e7f93738eec72a0e0a7385c0c669
RobinLiu <liulongyang0305@126.com>
2020-04-06 15:16:55 +0800

    Create 1404_NumberOfStepsToReduceNumberInBinaryRepresentationToOne.swift

---
+++ swift/String/1404_NumberOfStepsToReduceNumberInBinaryRepresentationToOne.swift
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+class Solution {
+    func numSteps(_ s: String) -> Int {
+        guard s.count > 1 else {
+            return 0
+        }
+        var ans = 0
+        let num = [Int](s.map { $0 == "0" ? 0 : 1}.reversed())
+        
+        let N = s.count
+        var carry = 0
+        
+        for i in 0..<N {
+            
+            let lastDigit = num[i] + carry
+            if lastDigit == 1 && i == N - 1 {
+                break
+            }
+            if lastDigit == 1 {
+                ans += 2
+                carry = 1
+            } else {
+                ans += 1
+                carry = lastDigit / 2
+            }
+        }
+        return ans
+    }
+}
