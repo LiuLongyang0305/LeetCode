b6fa608e5b55de322d66091435c183627c19afab
RobinLiu <liulongyang0305@126.com>
2020-06-06 23:05:17 +0800

    Update 22_GenerateParentheses.swift

--- swift/String/22_GenerateParentheses.swift
+++ swift/String/22_GenerateParentheses.swift
@@ -1,18 +1,25 @@
 // https://leetcode.com/problems/generate-parentheses/
 class Solution {
+    var map = [Int:[String]]()
     func generateParenthesis(_ n: Int) -> [String] {
+        guard n > 0 else {
+            map[0] = [""]
+            return [""]
+        }
+        if let parenthesis = map[n] {
+            return parenthesis
+        }
         var ans = [String]()
-        if n == 0 {
-            ans.append("")
-        } else {
-            for i in 0..<n {
-                for left in generateParenthesis(i) {
-                    for right in generateParenthesis(n - 1 - i) {
-                        ans.append("(" + left + ")" + right)
-                    }
+        
+        for i in 0..<n {
+            for left in generateParenthesis(i) {
+                for right in generateParenthesis(n - 1 - i) {
+                    ans.append("(" + left + ")" + right)
                 }
             }
         }
+        
+        map[n] = ans
         return ans
     }
 }
