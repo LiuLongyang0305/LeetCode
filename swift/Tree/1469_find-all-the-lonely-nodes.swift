c5f5fe985208359605a4279565cbfd8e1f3d55a9
RobinLiu <liulongyang0305@126.com>
2020-09-17 09:01:28 +0800

    Create 1469_find-all-the-lonely-nodes.swift

---
+++ swift/Tree/1469_find-all-the-lonely-nodes.swift
@@ -0,0 +1,37 @@
+//  https://leetcode.com/problems/find-all-the-lonely-nodes/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func getLonelyNodes(_ root: TreeNode?) -> [Int] {
+        var ans = [Int]()
+        func dfs(_ node: TreeNode?) {
+            guard let n = node else {
+                return
+            }
+            if nil == n.left && nil != n.right {
+                ans.append(n.right!.val)
+            }
+            if nil != n.left && nil == n.right {
+                ans.append(n.left!.val)
+            }
+            
+            dfs(n.left)
+            dfs(n.right)
+        }
+        dfs(root)
+        return ans
+    }
+}
