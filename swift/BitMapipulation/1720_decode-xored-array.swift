2a7ce427a6eb1e0fe96875c13cdf967af586283c
RobinLiu <liulongyang0305@126.com>
2021-01-20 16:19:25 +0800

    Create 1720_decode-xored-array.swift

---
+++ swift/BitMapipulation/1720_decode-xored-array.swift
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/decode-xored-array/
+class Solution {
+    func decode(_ encoded: [Int], _ first: Int) -> [Int] {
+        var ans = [first]
+        for num in encoded {
+            ans.append(ans.last! ^ num)
+        }
+        return ans
+    }
+}
