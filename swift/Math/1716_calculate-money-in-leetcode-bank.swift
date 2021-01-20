0db983a81378dfd4fe9e4d0013065455b1066649
RobinLiu <liulongyang0305@126.com>
2021-01-20 16:19:43 +0800

    Create 1716_calculate-money-in-leetcode-bank.swift

---
+++ swift/Math/1716_calculate-money-in-leetcode-bank.swift
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/calculate-money-in-leetcode-bank/
+class Solution {
+    func totalMoney(_ n: Int) -> Int {
+        let firstWeekMoney = (0...7).map { (0...$0).reduce(0) { $0 + $1}}
+        guard n >= 7 else {
+            return firstWeekMoney[n]
+        }
+        
+        let totalWeek = n / 7
+        let remainDays = n % 7
+        var ans = totalWeek * firstWeekMoney[7] + 7 * (0..<totalWeek).reduce(0, { $0 + $1})
+        ans += totalWeek * remainDays + firstWeekMoney[remainDays]
+        return ans
+    }
+}
