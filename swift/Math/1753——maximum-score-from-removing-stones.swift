dbf49b814497b873c8dc53a7464d437a5ee7254a
RobinLiu <liulongyang0305@126.com>
2021-02-09 00:04:07 +0800

    Create 1753——maximum-score-from-removing-stones.swift

---
+++ swift/Math/1753——maximum-score-from-removing-stones.swift
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/maximum-score-from-removing-stones/
+class Solution {
+    func maximumScore(_ a: Int, _ b: Int, _ c: Int) -> Int {
+        let s = a + b + c
+        return min(s - max(a,b,c),s >> 1)
+    }
+}
