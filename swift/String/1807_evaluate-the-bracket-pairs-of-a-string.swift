ff0ef3cf3064db5ddf032034bd3c3cac8b3e45d6
RobinLiu <liulongyang0305@126.com>
2021-03-29 16:04:38 +0800

    Update 1807_evaluate-the-bracket-pairs-of-a-string.swift

--- swift/String/1807_evaluate-the-bracket-pairs-of-a-string.swift
+++ swift/String/1807_evaluate-the-bracket-pairs-of-a-string.swift
@@ -25,3 +25,40 @@ class Solution {
         return ans
     }
 }
+
+class Solution {
+    func evaluate(_ s: String, _ knowledge: [[String]]) -> String {
+        var ans = ""
+        var map = [String:String]()
+        for k in knowledge {
+            map[k[0]] = k[1]
+        }
+        var leftIndex: String.Index? = nil
+        var rightIndex: String.Index? = nil
+        var idx = s.startIndex
+        while idx < s.endIndex {
+            if s[idx] == "(" {
+                if let r = rightIndex {
+                    ans.append(contentsOf: s[s.index(after: r)..<idx])
+                } else {
+                    ans.append(contentsOf: s[s.startIndex..<idx])
+                }
+                leftIndex = idx
+            } else if s[idx] == ")" {
+                let key = String(s[s.index(after: leftIndex!)..<idx])
+                ans.append(map[key] ?? "?")
+                rightIndex = idx
+            }
+            idx = s.index(after: idx)
+        }
+        if s[s.index(before: idx)] != ")" {
+            if let r = rightIndex {
+                ans.append(contentsOf: s[s.index(after: r)..<idx])
+            } else {
+                ans.append(contentsOf: s[s.startIndex..<idx])
+            }
+        }
+        return ans
+    }
+}
+
