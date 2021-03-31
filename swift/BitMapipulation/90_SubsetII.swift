e0163c011e98e440bb7d1aa4f9211bd36f120f40
RobinLiu <liulongyang0305@126.com>
2021-03-31 12:49:03 +0800

    Update 90_SubsetII.swift

--- swift/BitMapipulation/90_SubsetII.swift
+++ swift/BitMapipulation/90_SubsetII.swift
@@ -1,38 +1,19 @@
 //https://leetcode.com/problems/subsets-ii/
 class Solution {
+    private let bits = (0...10).map { 1 << $0}
     func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
-        var existed = Set<[Int]>()
-        var  ans = [[Int]]()
-        guard nums.count > 0 else {
-            return [[Int]]()
-        }
-        var numsCopy = nums.sorted()
-        let length  = Int(exactly: pow(Double(2), Double(nums.count)))!
-        for i in 0..<length {
-            let ones = getOnesBits(i)
-            var temp = [Int]()
-            if !ones.isEmpty {
-                for j in 0..<ones.count {
-                    if  ones[j] == 1 {
-                        temp.append(numsCopy[j])
-                    }
+        var ans = Set<[Int]>()
+        let N = nums.count
+        let sortedNumbers = nums.sorted()
+        (0...bits[N]).forEach { (v) in
+            var cur = [Int]()
+            for i in 0..<N {
+                if v & bits[i] != 0 {
+                    cur.append(sortedNumbers[i])
                 }
             }
-            if !existed.contains(temp) {
-                existed.insert(temp)
-                ans.append(temp)
-            }
+            ans.insert(cur)
         }
-        return ans
-    }
-    
-    private func getOnesBits(_ n: Int) -> [Int] {
-        var ans = [Int]()
-        var temp = n
-        while temp > 0 {
-            ans.append(temp & 1)
-            temp >>= 1
-        }
-        return ans
+        return [[Int]](ans)
     }
 }
