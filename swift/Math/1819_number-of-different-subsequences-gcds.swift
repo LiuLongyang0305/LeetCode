33794723cc76c6ac9a95400348439c2da5ab8a18
RobinLiu <liulongyang0305@126.com>
2021-04-07 11:14:53 +0800

    Create 1819_number-of-different-subsequences-gcds.swift

---
+++ swift/Math/1819_number-of-different-subsequences-gcds.swift
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/number-of-different-subsequences-gcds/
+class Solution {
+    func countDifferentSubsequenceGCDs(_ nums: [Int]) -> Int {
+        var cnt = 0
+        let max = nums.max()!
+        var numbers = Array<Bool>(repeating: false, count: max + 1)
+        for num in nums {
+            numbers[num] = true
+        }
+        for i in 1...max {
+            guard !numbers[i] else {
+                cnt += 1
+                continue
+            }
+            var g = 0
+            var b = i
+            while b <= max  {
+                if numbers[b] {
+                    g =  __gcd(g, b)
+                }
+                b += i
+            }
+            if g == i {
+                cnt += 1
+            }
+        }
+        return cnt
+    }
+    
+    private func __gcd(_ a: Int, _ b: Int) -> Int {
+        guard b != 0 else {
+            return a
+        }
+        return a % b == 0 ? b : __gcd(b, a % b)
+    }
+}
+
