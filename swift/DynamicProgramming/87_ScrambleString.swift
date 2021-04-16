c7f1f2e599b88e1b4c8e6bba833768335a28892b
RobinLiu <1285609104@qq.com>
2021-04-16 17:23:33 +0800

    Update 87_ScrambleString.swift

--- swift/DynamicProgramming/87_ScrambleString.swift
+++ swift/DynamicProgramming/87_ScrambleString.swift
@@ -7,10 +7,20 @@
     }
  }
  class Solution {
+    private struct State: Hashable {
+        var left:Substring
+        var right:Substring
+    }
+    private var memo = [State:Bool]()
     func isScramble(_ s1: String, _ s2: String) -> Bool {
-        return innerIsScramble( Substring(s1), Substring(s2))
+        memo = [:]
+      return innerIsScramble( Substring(s1), Substring(s2))
     }
     private func  innerIsScramble(_ s1: Substring, _ s2: Substring) -> Bool {
+        let key = State(left: s1, right: s2)
+        guard nil == memo[key] else {
+            return memo[key]!
+        }
         guard s1 != s2 else {
             return true
         }
@@ -20,9 +30,11 @@
         let N = s1.count
         for length in 1..<N {
             if (innerIsScramble(s1.prefix(length), s2.prefix(length)) && innerIsScramble(s1.suffix(N - length), s2.suffix(N - length))) || (innerIsScramble(s1.prefix(length), s2.suffix(length)) && innerIsScramble(s1.suffix(N - length), s2.prefix(N - length))) {
+                memo[key] = true
                 return true
             }
         }
+        memo[key] = false
         return false
     }
  }
