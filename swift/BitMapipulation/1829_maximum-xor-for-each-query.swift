ec4de61a23d27c0503b9ff1be5b74443caed4c04
RobinLiu <1285609104@qq.com>
2021-04-18 10:20:05 +0800

    Create 1829_maximum-xor-for-each-query.swift

---
+++ swift/BitMapipulation/1829_maximum-xor-for-each-query.swift
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/maximum-xor-for-each-query/
+class Solution {
+    private let bits = (0...20).map {1 << $0}
+    func getMaximumXor(_ nums: [Int], _ maximumBit: Int) -> [Int] {
+        var total = nums.reduce(0) {$0 ^ $1}
+        var ans = [Int]()
+        for idx in stride(from: nums.count - 1, through: 0, by: -1) {
+            var maxAns = 0
+            for bit in 0..<maximumBit {
+                if total & bits[bit] == 0 {
+                    maxAns |= bits[bit]
+                }
+            }
+            ans.append(maxAns)
+            total = total ^ nums[idx]
+        }
+        return ans
+    }
+}
+
