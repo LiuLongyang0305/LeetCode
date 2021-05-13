53630f47dc74104df546f6342bd3eb6b590e369e
RobinLiu <1285609104@qq.com>
2021-05-13 13:21:43 +0800

    Create 1854_maximum-population-year.swift

---
+++ swift/Array/1854_maximum-population-year.swift
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/maximum-population-year/
+class Solution {
+    func maximumPopulation(_ logs: [[Int]]) -> Int {
+        var lifePeriods = Array<Int>(repeating: 0, count: 105)
+        for log in logs {
+            lifePeriods[log[0] - 1950] += 1
+            lifePeriods[log[1] - 1950] -= 1
+        }
+        var total = 0
+        var ans: (year:Int,num: Int) = (-1,-1)
+        for year in 1950...2050 {
+            total += lifePeriods[year - 1950]
+            if total > ans.num {
+                ans.year = year
+                ans.num = total
+            }
+        }
+        return ans.year
+    }
+}
