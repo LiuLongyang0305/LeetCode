fc91cb6f57d6b7f8a92c44c25c09028b2f0f0fe3
LiuLongyang0305 <1285609104@qq.com>
2021-06-25 12:33:44 +0800

    Update 654_MaximumBinaryTree.swift

--- swift/Tree/654_MaximumBinaryTree.swift
+++ swift/Tree/654_MaximumBinaryTree.swift
@@ -34,3 +34,42 @@ class Q654_Solution {
         return root
     }
 }
+
+//单调栈
+class Solution {
+    func constructMaximumBinaryTree(_ nums: [Int]) -> TreeNode? {
+        guard !nums.isEmpty else {
+            return nil
+        }
+        var stack = [TreeNode]()
+        stack.append(TreeNode(Int.max))
+        for num in nums {
+            if num < stack.last!.val {
+                let node = TreeNode(num)
+                stack.last?.right = node
+                stack.append(node)
+            } else {
+                var lastNode: TreeNode? = nil
+                while !stack.isEmpty && stack.last!.val < num {
+                    lastNode = stack.removeLast()
+                }
+                let node = TreeNode(num)
+                node.left = lastNode
+                stack.last?.right = node
+                stack.append(node)
+            }
+            // show(stack.first)
+        }
+        return stack.first?.right
+    }
+    
+    private func show(_ root: TreeNode?) {
+        guard let r = root else {
+            return
+        }
+        print(r.val)
+        show(r.left)
+        show(r.right)
+    }
+}
+
