1195edcfdd19f33fdea8c5c1170f3d425ffb90bc
LiuLongyang0305 <1285609104@qq.com>
2021-06-25 14:57:23 +0800

    Update 496_NextGreaterElement.swift

--- swift/Stack/496_NextGreaterElement.swift
+++ swift/Stack/496_NextGreaterElement.swift
@@ -16,3 +16,18 @@ class Solution {
         return ans
     }
 }
+
+class Solution {
+    func nextGreaterElement(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        var  stack = [Int]()
+        var dic = [Int:Int]()
+        for num in nums2 {
+            while !stack.isEmpty && stack.last! < num {
+                dic[stack.removeLast()] = num
+            }
+            stack.append(num)
+        }
+        return nums1.map { dic[$0] ?? -1}
+    }
+}
+
