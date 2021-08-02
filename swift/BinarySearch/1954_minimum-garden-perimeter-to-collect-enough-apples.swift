4e529c940cab51ed80bb7017dbe7fce00bef576a
LiuLongyang0305 <1285609104@qq.com>
2021-08-02 15:40:38 +0800

    Create 1954_minimum-garden-perimeter-to-collect-enough-apples.swift

---
+++ swift/BinarySearch/1954_minimum-garden-perimeter-to-collect-enough-apples.swift
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/minimum-garden-perimeter-to-collect-enough-apples/
+class Solution {
+    func minimumPerimeter(_ neededApples: Int) -> Int {
+        var low = 1
+        var high = 1000000
+        while low < high {
+            let mid = (low + high) >> 1
+            let applesCnt = 2 * mid * (mid + 1) * (2 * mid + 1)
+            if applesCnt < neededApples {
+                low = mid + 1
+            } else {
+                high = mid
+            }
+        }
+        return 8 * low
+    }
+}
