5e6842be9b449464d2e8e90bfb951655945246eb
LiuLongyang0305 <43689684+LiuLongyang0305@users.noreply.github.com>
2021-08-09 16:04:29 +0800

    Create 300_longest-increasing-subsequence.swift

---
+++ swift/Greedy/300_longest-increasing-subsequence.swift
@@ -0,0 +1,33 @@
+// https://leetcode-cn.com/problems/longest-increasing-subsequence/
+class Solution {
+    func lengthOfLIS(_ nums: [Int]) -> Int {
+        let N = nums.count
+        guard N > 1 else {
+            return nums.isEmpty ? 0 : 1
+        }
+        var length = 1
+        var d = Array<Int>(repeating: 0, count: N + 1)
+        d[length] = nums[0]
+        for idx in 1..<N {
+            guard nums[idx] <= d[length] else {
+                length += 1
+                d[length] = nums[idx]
+                continue
+            }
+            var left = 1
+            var right = length
+            var pos = 0
+            while left <= right {
+                let mid = (left + right) >> 1
+                if d[mid] < nums[idx] {
+                    pos = mid
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            }
+            d[pos + 1] = nums[idx]
+        }
+        return length
+    }
+}
