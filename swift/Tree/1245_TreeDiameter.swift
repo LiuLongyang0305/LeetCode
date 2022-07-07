42b84cef1942cc7bad0fabd1a18eb3e385c83374
LiuLongyang0305 <43689684+LiuLongyang0305@users.noreply.github.com>
2022-07-07 16:29:57 +0800

    Update 1245_TreeDiameter.swift

--- swift/Tree/1245_TreeDiameter.swift
+++ swift/Tree/1245_TreeDiameter.swift
@@ -1,45 +1,44 @@
 //https://leetcode.com/contest/biweekly-contest-12/problems/tree-diameter/
+
 class Solution {
     func treeDiameter(_ edges: [[Int]]) -> Int {
-        var tree = [Int: Set<Int>]()
+
+        let N = edges.count + 1
+        guard !edges.isEmpty else {return 0}
+        var tree = [[Int]](repeating: [], count: N)
         for edge in edges {
-            if nil == tree[edge[0]]{
-                tree[edge[0]] = []
-            }
-            if nil == tree[edge[1]]{
-                tree[edge[1]] = []
-            }
-            tree[edge[0]]?.insert(edge[1])
-            tree[edge[1]]?.insert(edge[0])
+            tree[edge[0]].append(edge[1])
+            tree[edge[1]].append(edge[0])
         }
 
-        
-        func bfs(_ root: Int) -> (Int,Int) {
-            
-            var start = -1
-            var visited = Set<Int>()
+
+        func bfs(_ root: Int) -> (point:Int,depth:Int) {
+
+            var point = -1
+            var visited = [Bool](repeating: false, count: N)
             var depth = 0
-            
             var nodes = Array<Int>()
             nodes.append(root)
+            visited[root] = true
+
             while !nodes.isEmpty {
-                print(nodes)
-                start = nodes[0]
+                point = nodes[0]
                 depth += 1
-                let length = nodes.count
-                for _ in 0..<length {
-                    let node = nodes.removeFirst()
-                    visited.insert(node)
-                    for child in tree[node]! {
-                        if !visited.contains(child) {
-                            nodes.append(child)
+               var newQueue = [Int]()
+                for node in nodes {
+                    for child in tree[node] {
+                        guard !visited[child] else {
+                            continue
                         }
+                        newQueue.append(child)
+                        visited[child] = true
                     }
                 }
+                nodes = newQueue
             }
-            return (start,depth)
+            return (point,depth)
         }
-        
+
         return bfs(bfs(0).0).1 - 1
     }
 }
