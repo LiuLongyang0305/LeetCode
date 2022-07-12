52b7a9e9b0ad4222d60d03e573d86202f273fe9f
LiuLongyang0305 <43689684+LiuLongyang0305@users.noreply.github.com>
2022-07-12 17:53:11 +0800

    Update 1510_stone-game-iv.swift

--- swift/DynamicProgramming/1510_stone-game-iv.swift
+++ swift/DynamicProgramming/1510_stone-game-iv.swift
@@ -16,3 +16,39 @@ class Solution {
     }
 }
 
+ // https://leetcode.com/problems/stone-game-iv/
+class Solution {
+    let squeres = (1...316).map { $0 * $0}
+    let squeresSet = Set<Int>((1...316).map { $0 * $0})
+    func winnerSquareGame(_ n: Int) -> Bool {
+
+        var memo = [Bool?](repeating: nil, count: n + 2)
+        func dfs(_ remain: Int) -> Bool {
+
+            guard !squeresSet.contains(remain) else {
+                return true
+            }
+
+            if let v = memo[remain] {
+                return v
+            }
+
+            var ans = false
+            for squre in squeres {
+                guard squre < remain else {
+                    break
+                }
+                ans = !dfs(remain - squre)
+                guard !ans else {
+                    break
+                }
+            }
+
+            memo[remain] = ans
+            return ans
+        }
+
+
+        return dfs(n)
+    }
+}
