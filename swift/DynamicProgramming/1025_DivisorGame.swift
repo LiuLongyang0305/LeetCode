f173bd971bf02a234f6e6d328973f6e05529650b
LiuLongyang0305 <43689684+LiuLongyang0305@users.noreply.github.com>
2022-07-14 08:47:30 +0800

    Update 1025_DivisorGame.swift

--- swift/DynamicProgramming/1025_DivisorGame.swift
+++ swift/DynamicProgramming/1025_DivisorGame.swift
@@ -4,3 +4,42 @@ class Solution {
         return N % 2 == 0
     }
 }
+
+//https://leetcode.com/problems/divisor-game/
+class Solution {
+    func divisorGame(_ n: Int) -> Bool {
+
+        var memo = [Bool?](repeating: nil, count: n + 1)
+
+        func dfs(_ num: Int) -> Bool {
+            guard num > 1 else {
+                return false
+            }
+
+            if let v = memo[num] {
+                return v
+            }
+
+            var ans = false
+            var i = 1
+            while i <= num / i {
+                if num % i == 0  {
+                    ans = !dfs(num - i)
+                    guard !ans else {
+                       break
+                    }
+                    if i != 1 {
+                        ans = !dfs(num - num / i)
+                    }
+                    guard !ans else {
+                       break
+                    }
+                }
+                i += 1
+            }
+            memo[num] = ans
+            return ans
+        }
+        return dfs(n)
+    }
+}
