90d3ddcb5c440f4718f76482ff58bc465efe455d
LiuLongyang0305 <43689684+LiuLongyang0305@users.noreply.github.com>
2022-07-15 17:34:23 +0800

    Update 834_SumOfDistanceInTree.swift

--- swift/Tree/834_SumOfDistanceInTree.swift
+++ swift/Tree/834_SumOfDistanceInTree.swift
@@ -1,38 +1,39 @@
 //https://leetcode.com/problems/sum-of-distances-in-tree/
 class Solution {
-    var ans =  [Int]()
-    var count = [Int]()
-    var graph = [Set<Int>]()
-    var N: Int = -1
     func sumOfDistancesInTree(_ N: Int, _ edges: [[Int]]) -> [Int] {
-        self.N =  N
-        ans = Array<Int>(repeating: 0, count: N)
-        count = Array<Int>(repeating: 1, count: N)
-        graph = Array<Set<Int>>(repeating: Set<Int>(), count: N)
-        for edge in edges {
-            graph[edge[0]].insert(edge[1])
-            graph[edge[1]].insert(edge[0])
+
+        var graph = [[Int]](repeating: [], count: N)
+        for e in edges {
+            graph[e[0]].append(e[1])
+            graph[e[1]].append(e[0])
         }
-        dfs(root: 0, parent: -1)
-        dfs2(root: 0, parent: -1)
+
+        var ans = [Int](repeating: 0, count: N)
+        var countNodes = [Int](repeating: 1, count: N)
+
+
+        func  dfs(_ node: Int, _ parent: Int) {
+            for child in graph[node] {
+                guard parent != child else {continue}
+                dfs(child, node)
+                countNodes[node] += countNodes[child]
+                ans[node] += ans[child] + countNodes[child]
+            }
+        }
+
+
+        func dfs2(_ node: Int, _ parent: Int) {
+            for child in graph[node] {
+                guard child != parent else {continue}
+                ans[child] = ans[node] - countNodes[child] + N - countNodes[child]
+                dfs2(child, node)
+            }
+        }
+
+        dfs(0,-1)
+        dfs2(0,-1)
+
         return ans
-    }
-    
-    private func dfs(root: Int, parent: Int)  {
-        for child in graph[root] {
-            if child != parent  {
-                dfs(root: child, parent: root)
-                count[root] +=  count[child]
-                ans[root] +=  ans[child]  + count[child]
-            }
-        }
-    }
-    private func dfs2(root: Int, parent: Int)  {
-        for child in graph[root] {
-            if child != parent {
-                ans[child] = ans[root] - count[child] + N - count[child]
-                dfs2(root: child, parent: root)
-            }
-        }
+
     }
 }
