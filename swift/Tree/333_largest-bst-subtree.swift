253938f9db19a42c56765beaad722228240a9a46
LiuLongyang0305 <43689684+LiuLongyang0305@users.noreply.github.com>
2022-07-16 23:57:49 +0800

    Update 333_largest-bst-subtree.swift

--- swift/Tree/333_largest-bst-subtree.swift
+++ swift/Tree/333_largest-bst-subtree.swift
@@ -32,3 +32,48 @@ class Solution {
         return ([],false)
     }
 }
+
+
+class Solution {
+    private typealias Squre = (min:Int,max:Int,isBST:Bool,nodeCnt:Int)
+    func largestBSTSubtree(_ root: TreeNode?) -> Int {
+
+        var ans = 0
+
+        func dfs(_ node: TreeNode?) -> Squre? {
+            guard let node = node else {
+                return nil
+            }
+
+            var res: Squre = (0,0,false,0)
+            let leftAns = dfs(node.left)
+            let rightAns = dfs(node.right)
+            //叶子节点
+            if nil == leftAns && nil == rightAns {
+                res =  (node.val,node.val,true,1)
+            }
+
+            //仅有右子树
+            if nil == leftAns {
+                if let r = rightAns {
+                    res = (min(node.val, r.min),max(node.val, r.max),r.min > node.val && r.isBST,r.nodeCnt + 1)
+                }
+            }
+            if nil == rightAns {
+                if let l = leftAns {
+                    res = (min(node.val, l.min),max(node.val, l.max),l.isBST && l.max < node.val,l.nodeCnt + 1)
+                }
+            }
+            if let l = leftAns, let r = rightAns {
+                res = (min(l.min, r.min,node.val),max(l.max, node.val,r.max),l.isBST && r.isBST && l.max < node.val && r.min > node.val,l.nodeCnt + r.nodeCnt + 1)
+            }
+            if res.isBST {
+                ans = max(ans, res.nodeCnt)
+            }
+            return res
+        }
+
+        dfs(root)
+        return ans
+    }
+}
