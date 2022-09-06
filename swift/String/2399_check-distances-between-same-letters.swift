76e3119b77bc81e41f5695acc7df64d3757424cb
LiuLongyang0305 <43689684+LiuLongyang0305@users.noreply.github.com>
2022-09-06 15:27:35 +0800

    Create 2399_check-distances-between-same-letters.swift

---
+++ swift/String/2399_check-distances-between-same-letters.swift
@@ -0,0 +1,17 @@
+ // https://leetcode.com/problems/check-distances-between-same-letters/
+class Solution {
+    func checkDistances(_ s: String, _ distance: [Int]) -> Bool {
+        let values = s.map { Int($0.asciiValue ?? 0) - 97}
+        var last = [Int](repeating: -1, count: 26)
+        let N = values.count
+        for i in 0..<N {
+            let idx = values[i]
+            if last[idx] != -1 {
+                guard distance[idx] == i - last[idx] - 1 else {return false}
+            } else {
+                last[idx] = i
+            }
+        }
+        return true
+    }
+}
