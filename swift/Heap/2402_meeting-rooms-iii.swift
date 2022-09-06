1911af2c98f5519f450b3065ce11b12b8078d29c
LiuLongyang0305 <43689684+LiuLongyang0305@users.noreply.github.com>
2022-09-06 15:27:31 +0800

    Create 2402_meeting-rooms-iii.swift

---
+++ swift/Heap/2402_meeting-rooms-iii.swift
@@ -0,0 +1,84 @@
+// https://leetcode.com/problems/meeting-rooms-iii/
+import CoreFoundation
+// import Accelerate
+
+struct MeetingRoom {
+    var num: Int
+    var emptyTime:Int
+}
+
+extension CFBinaryHeap {
+    static public func getMinHeapOfMeetingRoom() -> CFBinaryHeap {
+        var callbacks = CFBinaryHeapCallBacks()
+        callbacks.compare = {  a, b,_ in
+            let x = a!.load(as: MeetingRoom.self)
+            let y = b!.load(as: MeetingRoom.self)
+            guard x.emptyTime != y.emptyTime else {
+                return x.num < y.num ? .compareLessThan : .compareGreaterThan
+            }
+            return x.emptyTime < y.emptyTime ? .compareLessThan : .compareGreaterThan
+        }
+        return CFBinaryHeapCreate(nil, 0, &callbacks, nil)
+    }
+}
+
+class Solution {
+    func mostBooked(_ n: Int, _ meetings: [[Int]]) -> Int {
+        let sortedMeetings = meetings.sorted { m1, m2 in
+            m1[0] < m2[0]
+        }
+        print(sortedMeetings)
+
+
+
+
+        var counter = [Int](repeating: 0, count: n)
+        let busyHeap = CFBinaryHeap.getMinHeapOfMeetingRoom()
+        let emptyHeap = CFBinaryHeap.getMinHeapOfMeetingRoom()
+        var pointer : UnsafeMutablePointer<MeetingRoom> = .allocate(capacity: 1)
+
+
+        func add(_ heap: CFBinaryHeap, _ val: MeetingRoom) {
+            pointer = .allocate(capacity: 1)
+            pointer.initialize(to: val)
+            CFBinaryHeapAddValue(heap, pointer)
+
+        }
+
+        for i in 0..<n {
+            add(emptyHeap, MeetingRoom(num: i, emptyTime: 0))
+        }
+
+        for meet in sortedMeetings {
+
+            let (startTime,endTime) = (meet[0],meet[1])
+            while CFBinaryHeapGetCount(busyHeap) > 0 {
+                var top = CFBinaryHeapGetMinimum(busyHeap).load(as: MeetingRoom.self)
+                guard top.emptyTime <= startTime else {
+                    break
+                }
+                CFBinaryHeapRemoveMinimumValue(busyHeap)
+                top.emptyTime = 0
+                add(emptyHeap, top)
+            }
+
+            if CFBinaryHeapGetCount(emptyHeap) > 0 {
+                var top = CFBinaryHeapGetMinimum(emptyHeap).load(as: MeetingRoom.self)
+                CFBinaryHeapRemoveMinimumValue(emptyHeap)
+                counter[top.num] += 1
+                top.emptyTime = endTime
+               add(busyHeap, top)
+            } else {
+                var top = CFBinaryHeapGetMinimum(busyHeap).load(as: MeetingRoom.self)
+                CFBinaryHeapRemoveMinimumValue(busyHeap)
+                counter[top.num] += 1
+                top.emptyTime += endTime - startTime
+                add(busyHeap, top)
+            }
+        }
+
+        let maxCnt = counter.max() ?? -1
+        return counter.firstIndex(of: maxCnt) ?? -1
+
+    }
+}
