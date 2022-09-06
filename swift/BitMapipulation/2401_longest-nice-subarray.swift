7c2852259d9962fe928922a456b7dbd0384e377a
LiuLongyang0305 <43689684+LiuLongyang0305@users.noreply.github.com>
2022-09-06 15:27:25 +0800

    Create 2401_longest-nice-subarray.swift

---
+++ swift/BitMapipulation/2401_longest-nice-subarray.swift
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/longest-nice-subarray/
+class Solution {
+    func longestNiceSubarray(_ nums: [Int]) -> Int {
+
+        var ans = 1
+        let N = nums.count
+        var left = 0
+        var right = 0
+        var mask = 0
+        while left < N {
+            while  right < N && mask & nums[right] == 0 {
+                mask |= nums[right]
+                right += 1
+            }
+            ans = max(ans, right - left)
+            mask ^= nums[left]
+            left += 1
+        }
+
+        return ans
+    }
+}
