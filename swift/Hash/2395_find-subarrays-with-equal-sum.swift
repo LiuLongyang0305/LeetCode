61af3821f4b6ac6b7af6709f58136410ca084a72
LiuLongyang0305 <43689684+LiuLongyang0305@users.noreply.github.com>
2022-09-06 15:27:28 +0800

    Create 2395_find-subarrays-with-equal-sum.swift

---
+++ swift/Hash/2395_find-subarrays-with-equal-sum.swift
@@ -0,0 +1,15 @@
+//  https://leetcode.com/problems/find-subarrays-with-equal-sum/
+class Solution {
+    func findSubarrays(_ nums: [Int]) -> Bool {
+        let N = nums.count
+        var set = Set<Int>()
+        for i in 1..<N {
+            let t = nums[i] + nums[i - 1]
+            guard !set.contains(t) else {
+                return true
+            }
+            set.insert(t)
+        }
+        return false
+    }
+}
