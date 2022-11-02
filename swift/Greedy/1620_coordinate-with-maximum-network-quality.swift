c824fd626ba1af42b47188e7d23e6757d05b5a7b
LiuLongyang0305 <43689684+LiuLongyang0305@users.noreply.github.com>
2022-11-02 17:35:19 +0800

    Update 1620_coordinate-with-maximum-network-quality.swift

--- swift/Greedy/1620_coordinate-with-maximum-network-quality.swift
+++ swift/Greedy/1620_coordinate-with-maximum-network-quality.swift
@@ -1,11 +1,14 @@
 // https://leetcode.com/problems/coordinate-with-maximum-network-quality/
 class Solution {
-    func bestCoordinate(_ towers: [[Int]], _ radius: Int) -> [Int] {
+    func bestCoordinate(_ _towers: [[Int]], _ radius: Int) -> [Int] {
+        let towers = _towers.filter { $0[2] > 0}
+        guard !towers.isEmpty else {return [0,0]}
         var quality = [[Int]:Int]()
         let radiusPower2 = radius * radius
         for tower in towers {
             let tx = tower[0]
             let ty = tower[1]
+
             for r in (tx - radius)...(tx + radius) {
                 for c in (ty - radius)...(ty + radius) {
                     let disPower2 = (tx - r) * (tx - r) + (ty - c) * (ty - c)
@@ -17,7 +20,7 @@ class Solution {
                         } else {
                             quality[key] = curQ
                         }
-                        
+
                     }
                 }
             }
@@ -25,19 +28,20 @@ class Solution {
         let maxQ = quality.values.max()!
         var ans = [Int]()
         for (key,q) in quality {
-            guard q == maxQ else {
-                continue
+            if q == maxQ {
+                if ans.isEmpty {
+                    ans = key
+                } else {
+                    if ans[0] == key[0] {
+                        if ans[1] > key[1] {
+                            ans = key
+                        }
+                    } else if ans[0] > key[0] {
+                        ans = key
+                    }
+                }
             }
-            guard !ans.isEmpty else {
-                ans = key
-                continue
-            }
-            guard !(ans[0] < key[0] || (ans[0] == key[0] && ans[1] < key[1])) else {
-                continue
-            }
-            ans = key
         }
         return ans
     }
 }
-
